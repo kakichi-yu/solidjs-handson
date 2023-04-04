601fcc4a137cee12595d266368a1a78a1dd1096c
Uchan <shocking.white.0103@gmail.com>
2023-04-05 08:18:14 +0900

    hard cord

--- task-tracker/src/App.tsx
+++ task-tracker/src/App.tsx
@@ -5,21 +5,25 @@ import styles from './App.module.css';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class="container mt-5 text-center">
+      <h1 class="mb-4">What TODO!</h1>
+
+      <form class="mb-5 row row-cols-2 justify-content-center">
+        <input type="text" class="input-group-text p-1 w-25" placeholder="Add task here..." id="taskInput" required />
+
+        <button class="btn btn-primary ms-3 w-auto" type="submit">
+          Add task
+        </button>
+      </form>
+
+      <div>
+        <h4 class="text-muted mb-4">Tasks</h4>
+        <div class="row row-cols-3 mb-3 justify-content-center">
+          <button class="btn btn-danger w-auto">X</button>
+          <div class="bg-light p-2 mx-2">Push code to GitHub</div>
+          <input type="checkbox" role="button" class="form-check-input h-auto px-3" />
+        </div>
+      </div>
     </div>
   );
 };
