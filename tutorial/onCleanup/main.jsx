a1a530a6c2329ea169fffda0afcc92c7a8b7188f
Uchan <shocking.white.0103@gmail.com>
2023-04-15 08:49:35 +0900

    onCleanup binding

---
+++ tutorial/onCleanup/main.jsx
@@ -0,0 +1,15 @@
+import { render } from "solid-js/web";
+import { createSignal, onCleanup } from "solid-js";
+
+function Counter() {
+  const [count, setCount] = createSignal(0);
+
+  // setInterval(() => setCount(count() + 1), 1000);
+
+  const timer = setInterval(() => setCount(count() + 1), 1000);
+  onCleanup(() => clearInterval(timer));
+
+  return <div>Count: {count()}</div>;
+}
+
+render(() => <Counter />, document.getElementById("app"));
