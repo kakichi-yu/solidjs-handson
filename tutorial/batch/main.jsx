b80cbeccee7b0a1dce4f10f34257a2242d02f1be
Uchan <shocking.white.0103@gmail.com>
2023-04-23 09:32:35 +0900

    reactivility

---
+++ tutorial/batch/main.jsx
@@ -0,0 +1,24 @@
+import { render } from "solid-js/web";
+import { createSignal, batch } from "solid-js";
+
+const App = () => {
+  const [firstName, setFirstName] = createSignal("John");
+  const [lastName, setLastName] = createSignal("Smith");
+  const fullName = () => {
+    console.log("Running FullName");
+    return `${firstName()} ${lastName()}`;
+  };
+  const updateNames = () => {
+    console.log("Button Clicked");
+    // setFirstName(firstName() + "n");
+    // setLastName(lastName() + "!");
+    batch(() => {
+      setFirstName(firstName() + "n");
+      setLastName(lastName() + "!");
+    });
+  };
+
+  return <button onClick={updateNames}>My name is {fullName()}</button>;
+};
+
+render(App, document.getElementById("app"));
